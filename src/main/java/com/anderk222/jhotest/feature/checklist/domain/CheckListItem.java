bc6d0126467df3490b2a8741c66c9b23e33082fb
anderk222 <anderk222@gmail.com>
2023-08-31 16:55:57 -0500

    add test-case entity

--- src/main/java/com/anderk222/jhotest/feature/checklist/domain/CheckListItem.java
+++ src/main/java/com/anderk222/jhotest/feature/checklist/domain/CheckListItem.java
@@ -4,14 +4,22 @@
  */
 package com.anderk222.jhotest.feature.checklist.domain;
 
+import com.anderk222.jhotest.feature.testcase.domain.TestCase;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 /**
  *
@@ -20,20 +28,29 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="item_check_list")
+@Table(name = "item_check_list")
 public class CheckListItem {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
-    
+
     @Column(length = 255, nullable = false)
     private String question;
-    
+    @Lob
+    @Column(columnDefinition = "text")
+    private String parameters;
+
     private boolean answer;
-    
+
+    private boolean passed;
     @Lob
     @Column(columnDefinition = "text")
     private String comment;
     
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(referencedColumnName = "id", name = "check_list_item")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<TestCase> testCases;
+
 }
---
+++ src/main/java/com/anderk222/jhotest/feature/testcase/domain/TestCase.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anderk222.jhotest.feature.testcase.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+/**
+ *
+ * @author tanki
+ */
+
+@Entity
+@Data
+@Table(name="test_case")
+public class TestCase {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    
+    @Column(length = 200, nullable = false)
+    private String name;
+    @Lob
+    @Column(columnDefinition = "text")
+    private String detail;
+    
+    @Lob
+    @Column(columnDefinition = "text")
+    private String parameters;
+    
+    private boolean passed;
+    
+}
